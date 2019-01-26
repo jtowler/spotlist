ea7eba98df7a73c6002bcd2f1626f81f5d7c7515
themother <jacktowleretc@hotmail.com>
2019-01-26 18:47:14 +0000

    setlist by date method

--- setlist/client.py
+++ setlist/client.py
@@ -10,15 +10,35 @@ class SetlistClient:
         self._headers = {'x-api-key': api_key,
                          'Accept': 'application/json'}
 
-    def get_latest_setlist_for_artist(self, artist) -> List[str]:
+    def get_latest_setlist_for_artist(self, artist: str) -> List[str]:
         mbid = self.get_artist_id(artist)
         query = f"artist/{mbid}/setlists?p=1"
         response = self._query(query)
         sets = response['setlist'][0]['sets']['set']
+        songs = self._setlist_to_set(sets)
+        return songs
+
+    def get_specific_setlist_for_artist(self, artist: str, date: str) -> List[str]:
+        mbid = self.get_artist_id(artist)
+        p = 1
+        while True:
+            query = f"artist/{mbid}/setlists?p={p}"
+            response = self._query(query)
+            for st in response['setlist']:
+                if st['eventDate'] == date:
+                    songs = self._setlist_to_set(st['sets']['set'])
+                    return songs
+                elif st['eventDate'] < date:
+                    return []
+            p += 1
+
+    @staticmethod
+    def _setlist_to_set(setlist: dict) -> List[str]:
         songs = []
-        for st in sets:
+        for st in setlist:
             for song in st['song']:
-                songs.append(song['name'])
+                if song not in songs:
+                    songs.append(song['name'])
         return songs
 
     def get_artist_id(self, artist: str) -> str:
