09109c441b2efa79c6eec757240fe7c84afe94fa
themother <jacktowleretc@hotmail.com>
2019-01-26 17:39:01 +0000

    get latest setlist method

--- setlist/client.py
+++ setlist/client.py
@@ -1,4 +1,5 @@
 import requests as re
+from typing import List
 
 
 class SetlistClient:
@@ -7,7 +8,18 @@ class SetlistClient:
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
         self._headers = {'x-api-key': api_key,
-                        'Accept': 'application/json'}
+                         'Accept': 'application/json'}
+
+    def get_latest_setlist_for_artist(self, artist) -> List[str]:
+        mbid = self.get_artist_id(artist)
+        query = f"artist/{mbid}/setlists?p=1"
+        response = self._query(query)
+        sets = response['setlist'][0]['sets']['set']
+        songs = []
+        for st in sets:
+            for song in st['song']:
+                songs.append(song['name'])
+        return songs
 
     def get_artist_id(self, artist: str) -> str:
         query = f"search/artists?p=1&artistName={artist}&sort=relevance"
